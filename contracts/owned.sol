fd09938ae4ca4b36126e1c60de5fffe1cc5bb520
nerjs <karifan.stap@gmail.com>
2018-09-11 16:06:07 +0300

    owned

---
+++ contracts/owned.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.0;
+
+contract Owned {
+    address owner;
+    address newOwner;
+    
+    modifier onlyOwner() {
+        require(owner == msg.sender);
+        _;
+    }
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    function transferOwner(address _newOwner) public onlyOwner {
+        newOwner = _newOwner;
+    }
+    
+    function confirmOwner() public {
+        if (newOwner == msg.sender) {
+            owner = msg.sender;
+        }
+    } 
+    
+    function testOwner() view public onlyOwner returns(string) {
+        return "Ok!";
+    }
+    
+}
